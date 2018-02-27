b4d630b7304036f6fd9816eeeb0ac781e77a6c05
ZhangXueSara <879290789@qq.com>
2018-02-27 10:19:45 +0800

    Add files via upload

---
+++ signViewController.h
@@ -0,0 +1,27 @@
+//
+//  signViewController.h
+//  draw
+//
+//  Created by mac on 2017/5/11.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@protocol signViewControllerDelegate <NSObject>
+
+- (void)showImage:(UIImage *)image;
+
+@end
+
+
+
+
+@interface signViewController : UIViewController
+
+//设置代理  弱引用
+@property (nonatomic,weak) id <signViewControllerDelegate> delegate;
+
+
+
+
+@end

0aecd2fe5177d1dede84e933a20ff3e45218a189
ZhangXueSara <879290789@qq.com>
2018-02-27 10:20:18 +0800

    Update signViewController.h

--- HandwritingSignature/QianMing/signViewController.h
+++ HandwritingSignature/QianMing/signViewController.h
@@ -19,7 +19,7 @@
 @interface signViewController : UIViewController
 
 //设置代理  弱引用
-@property (nonatomic,assign) id <signViewControllerDelegate> delegate;
+@property (nonatomic,weak) id <signViewControllerDelegate> delegate;
 
 
 
